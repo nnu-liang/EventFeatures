dfc188c16629f05ce328a881426d3b4314671fcc
liang <xl2662460419@qq.com>
2024-03-31 15:51:23 +0800

    impr(run_mg.sh):Add madspin

--- tools/run_mg.sh
+++ tools/run_mg.sh
@@ -1,5 +1,5 @@
-if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <process> <output_dir> <process_name> <launch_times> <root_file_dir>"
+if [ "$#" -ne 7 ]; then
+    echo "Usage: $0 <process> <output_dir> <process_name> <launch_times> <root_file_dir> <madspin> <madspin_card_path>"
     exit 1
 fi
 
@@ -9,6 +9,8 @@ OUTPUT_DIR="$2"
 PROCESS_NAME="$3"
 N="$4"
 ROOT_FILE_DIR="$5"
+MADSPIN="$6"
+MADSPIN_CARD_PATH="$7"
 DELPHES_CARD_PATH="/home/liang/Workingspace/Tagging/inputs/delphes_card_CMS.dat"
 RUN_CARD_PATH="/home/liang/Workingspace/Tagging/inputs/run_card.dat"
 
@@ -49,7 +51,20 @@ fi
 for ((i=1; i<=$N; i++))
 do
     if [ $i -eq 1 ]; then
-        ./bin/mg5_aMC<<EOF
+        if [ "$MADSPIN" -eq 1 ]; then
+            ./bin/mg5_aMC<<EOF
+launch $PROCESS_DIR
+1
+2
+4
+done
+$MADSPIN_CARD_PATH
+$DELPHES_CARD_PATH
+$RUN_CARD_PATH
+exit
+EOF
+        else
+            ./bin/mg5_aMC<<EOF
 launch $PROCESS_DIR
 1
 2
@@ -58,27 +73,39 @@ $DELPHES_CARD_PATH
 $RUN_CARD_PATH
 exit
 EOF
+        fi
     else
-        ./bin/mg5_aMC<<EOF
+        if [ "$MADSPIN" -eq 1 ]; then
+            ./bin/mg5_aMC<<EOF
+launch $PROCESS_DIR
+done
+$MADSPIN_CARD_PATH
+$DELPHES_CARD_PATH
+$RUN_CARD_PATH
+exit
+EOF
+        else
+            ./bin/mg5_aMC<<EOF
 launch $PROCESS_DIR
 done
 $DELPHES_CARD_PATH
 $RUN_CARD_PATH
 exit
 EOF
+        fi
     fi
     RUN_DIR="$PROCESS_DIR/Events/run_01/"
-if [ -d "$RUN_DIR" ]; then
-    cd $RUN_DIR
-    if [ -f "tag_1_delphes_events.root" ]; then
-        mv tag_1_delphes_events.root "${ROOT_FILE_DIR}/${PROCESS_NAME}_$(printf "%02d" $ROOT_FILE_NUM).root"
-        let ROOT_FILE_NUM+=1
+    if [ -d "$RUN_DIR" ]; then
+        cd $RUN_DIR
+        if [ -f "tag_1_delphes_events.root" ]; then
+            mv tag_1_delphes_events.root "${ROOT_FILE_DIR}/${PROCESS_NAME}_$(printf "%02d" $ROOT_FILE_NUM).root"
+            let ROOT_FILE_NUM+=1
+        fi
+        cd -  
+        rm -f "${PROCESS_DIR}/HTML/results.pkl"
+        find "${PROCESS_DIR}/HTML/" -type d -name "*run*" -exec rm -rf {} +
+        find "${PROCESS_DIR}/Events/" -mindepth 1 -type d -exec rm -rf {} +
     fi
-    cd -  
-    rm -f "${PROCESS_DIR}/HTML/results.pkl"
-    find "${PROCESS_DIR}/HTML/" -type d -name "*run*" -exec rm -rf {} +
-    find "${PROCESS_DIR}/Events/" -mindepth 1 -type d -exec rm -rf {} +
-fi
 done
 rm -rf "${PROCESS_DIR}"
 echo "完成：数据已在 $PROCESS_DIR 中生成，tag_1_delphes_events.root 文件已移至 $ROOT_FILE_DIR"
