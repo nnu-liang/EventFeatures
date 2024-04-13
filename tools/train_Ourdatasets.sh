75d6ae0b4e15483963de1ad3de0ab3a0ab5c67f8
liang <xl2662460419@qq.com>
2024-04-13 14:52:42 +0800

    impr(train_Ourdatasets.sh):Define the same path as a constant variable

--- tools/train_Ourdatasets.sh
+++ tools/train_Ourdatasets.sh
@@ -4,31 +4,37 @@ log_name=$3
 tensorboard_run_name=$4
 model_prefix_name=$5
 
-data_config="/home/lxiao/Workingspace/PartTransformer/particle_transformer-main/data/JetClass/JetClass_full.yaml"
-network_config="/home/lxiao/Workingspace/PartTransformer/particle_transformer-main/networks/example_ParticleTransformer.py"
-model_prefix="/home/lxiao/Workingspace/PartTransformer/particle_transformer-main/models/${model_prefix_name}.pt"
-model_weights="/home/lxiao/Workingspace/PartTransformer/particle_transformer-main/models/ParT_full.pt"
-log_path="/home/lxiao/Workingspace/PartTransformer/particle_transformer-main/logs/${log_name}.log"
-tensorboard_path="/home/lxiao/Workingspace/PartTransformer/particle_transformer-main/runs/${tensorboard_run_name}"
+DATA_DIR_TRAIN="/data/lxiao/JetClass_add_label_Zbb/train_25"
+DATA_DIR_VAL="/data/lxiao/JetClass_add_label_Zbb/val_25"
+DATA_DIR_TEST="/data/lxiao/JetClass_add_label_Zbb/test_25"
+DATA_DIR_OURDATASETS="/data/lxiao/Our_train_datasets"
+Tagging_DIR="/home/lxiao/Tagging"
+
+data_config="${Tagging_DIR}/data_config/JetClass_full.yaml"
+network_config="${Tagging_DIR}/networks/example_ParticleTransformer.py"
+model_prefix="${Tagging_DIR}/models/${model_prefix_name}.pt"
+model_weights="${Tagging_DIR}/models/ParT_full.pt"
+log_path="${Tagging_DIR}/logs/${log_name}.log"
+tensorboard_path="${Tagging_DIR}/runs/${tensorboard_run_name}"
 
 weaver --data-train \
-    "HToBB:/data/lxiao/JetClass_add_label_Zbb/train_25/HToBB*.root" \
-    "TTBar:/data/lxiao/JetClass_add_label_Zbb/train_25/TTBar_*.root" \
-    "TTBarLep:/data/lxiao/JetClass_add_label_Zbb/train_25/TTBarLep_*.root" \
-    "ZJetsToNuNu:/data/lxiao/JetClass_add_label_Zbb/train_25/ZJetsToNuNu_*.root" \
-    "Zbb:/data/lxiao/Our_train_datasets/Zbb_train_ParTF/Zbb*.root"\
+    "HToBB:${DATA_DIR_TRAIN}/HToBB*.root" \
+    "TTBar:${DATA_DIR_TRAIN}/TTBar_*.root" \
+    "TTBarLep:${DATA_DIR_TRAIN}/TTBarLep_*.root" \
+    "ZJetsToNuNu:${DATA_DIR_TRAIN}/ZJetsToNuNu_*.root" \
+    "Zbb:${DATA_DIR_OURDATASETS}/Zbb_train_ParTF/Zbb*.root"\
     --data-val \
-    "HToBB:/data/lxiao/JetClass_add_label_Zbb/val_25/HToBB*.root" \
-    "TTBar:/data/lxiao/JetClass_add_label_Zbb/val_25/TTBar_*.root" \
-    "TTBarLep:/data/lxiao/JetClass_add_label_Zbb/val_25/TTBarLep_*.root" \
-    "ZJetsToNuNu:/data/lxiao/JetClass_add_label_Zbb/val_25/ZJetsToNuNu_*.root" \
-    "Zbb:/data/lxiao/Our_train_datasets/Zbb_val_ParTF/Zbb*.root"\
+    "HToBB:${DATA_DIR_VAL}/HToBB*.root" \
+    "TTBar:${DATA_DIR_VAL}/TTBar_*.root" \
+    "TTBarLep:${DATA_DIR_VAL}/TTBarLep_*.root" \
+    "ZJetsToNuNu:${DATA_DIR_VAL}/ZJetsToNuNu_*.root" \
+    "Zbb:${DATA_DIR_OURDATASETS}/Zbb_val_ParTF/Zbb*.root"\
     --data-test \
-    "HToBB:/data/lxiao/JetClass_add_label_Zbb/test_25/HToBB_*.root" \
-    "TTBar:/data/lxiao/JetClass_add_label_Zbb/test_25/TTBar_*.root" \
-    "TTBarLep:/data/lxiao/JetClass_add_label_Zbb/test_25/TTBarLep_*.root" \
-    "ZJetsToNuNu:/data/lxiao/JetClass_add_label_Zbb/test_25/ZJetsToNuNu_*.root" \
-    "Zbb:/data/lxiao/Our_train_datasets/Zbb_test_ParTF/Zbb*.root"\
+    "HToBB:${DATA_DIR_TEST}/HToBB_*.root" \
+    "TTBar:${DATA_DIR_TEST}/TTBar_*.root" \
+    "TTBarLep:${DATA_DIR_TEST}/TTBarLep_*.root" \
+    "ZJetsToNuNu:${DATA_DIR_TEST}/ZJetsToNuNu_*.root" \
+    "Zbb:${DATA_DIR_OURDATASETS}/Zbb_test_ParTF/Zbb*.root"\
     --data-config $data_config \
     --network-config $network_config \
     --model-prefix $model_prefix \
