582fcecbd75546049423e68dc0315a5c03db26ee
liang <xl2662460419@qq.com>
2024-04-19 15:55:44 +0800

    impr(Batch_conversion_root_files.sh):parallel processing

--- tools/Batch_conversion_root_files.sh
+++ tools/Batch_conversion_root_files.sh
@@ -7,11 +7,14 @@ OUTPUT_DIR="/data/lxiao/Our_train_datasets/Zbb_train_ParTF"
 
 mkdir -p "$OUTPUT_DIR"
 
+max_jobs=30
+
 for FILE in "$INPUT_DIR"/*.root; do
     FILENAME=$(basename -- "$FILE")
     OUTPUT_FILE="$OUTPUT_DIR/${FILENAME%.root}_ParTF.root"
-    ./Root2ParTFormat.x "$LABEL" "$CONFIG_FILE" "$OUTPUT_FILE" "$FILE"
+    ./Root2ParTFormat.x "$LABEL" "$CONFIG_FILE" "$OUTPUT_FILE" "$FILE" &
+    (( $(jobs -r | wc -l) >= max_jobs )) && wait -n
 done
-
+wait
 echo "所有文件处理完成。"
 
